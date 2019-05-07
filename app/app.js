d25b155bf4b4c5d196a62d2e63819cef5890b068
mzaini30 <muhzaini30@gmail.com>
2019-05-07 21:38:31 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
-$('.bukan-input').each(function(){
-	var cleave = new Cleave(this, {
-		prefix: 'Rp ',
-	    numeral: true,
-	    delimiter: '.',
-	    numeralThousandsGroupStyle: 'thousand'
-	});
-	$(this).width($(this).parent().width())
-})
+mainkan_cleave = function(){
+	$('.bukan-input').each(function(){
+		var cleave = new Cleave(this, {
+			prefix: 'Rp ',
+		    numeral: true,
+		    delimiter: '.',
+		    numeralThousandsGroupStyle: 'thousand'
+		});
+		$(this).width($(this).parent().width())
+	})
+}
+mainkan_cleave()
 
 $('.grid').imagesLoaded( function() {
 	$('.grid').masonry({
@@ -22,7 +25,32 @@ $('.grid').imagesLoaded( function() {
 // })
 
 hitung = function(){
-
+	data_pembelian = []
+	$('.ini-dihitung').each(function(){
+		// data_pembelian.push(1)
+		data_pembelian.push({
+			'nama': $(this).find('.judul').html(),
+			'harga': $(this).find('.harga').attr('data-harga'),
+			'banyaknya': $(this).find('.banyaknya').val()
+		})
+		// console.log(data_pembelian)
+		hasil = 0
+		for (n in data_pembelian){
+			hasil += (parseInt(data_pembelian[n].harga) * parseInt(data_pembelian[n].banyaknya))
+		}
+		$('.jumlah-semua').val(hasil)
+		$('.jumlah-semua').each(function(){
+			var cleave = new Cleave(this, {
+				prefix: 'Rp ',
+			    numeral: true,
+			    delimiter: '.',
+			    numeralThousandsGroupStyle: 'thousand'
+			});
+			$(this).width($('.container').width())
+		})
+		// mainkan_cleave()
+		// console.log(hasil)
+	})
 }
 
 $('input').on('keyup', function(){
@@ -30,6 +58,7 @@ $('input').on('keyup', function(){
 		if ($(this).find('.banyaknya').val() != ''){
 			// ini kalau banyaknya diisi
 			$(this).addClass('ini-dihitung')
+			$(this).parent().addClass('blok')
 			hitung()
 			// $(this).css({
 			// 	'background': 'aqua'
@@ -37,6 +66,7 @@ $('input').on('keyup', function(){
 		} else {
 			// ini kalau banyaknya kosong
 			$(this).removeClass('ini-dihitung')
+			$(this).parent().removeClass('blok')
 			hitung()
 			// $(this).css({
 			// 	'background': 'transparent'
